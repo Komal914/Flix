1bdd9ade9979cb022668bf455a3e4b9515b9ef1b
Komal Kaur <komalpreet.kaur28@myhunter.cuny.edu>
2021-02-17 01:08:14 -0500

    added poster view + backdrop images onto moviedetalsscreen

--- Flix/MovieDetailsViewController.swift
+++ Flix/MovieDetailsViewController.swift
@@ -6,15 +6,35 @@
 //
 
 import UIKit
-
+import AlamofireImage
 class MovieDetailsViewController: UIViewController {
+
+    @IBOutlet weak var synopsisLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var posterView: UIImageView!
+    @IBOutlet weak var backdropView: UIImageView!
     var movie: [String: Any]!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        print(movie["title"])
+        titleLabel.text = movie["title"] as? String
+        titleLabel.sizeToFit()
+        synopsisLabel.text = movie["overview"] as? String
+        synopsisLabel.sizeToFit()
+        
+        let baseUrl = "http://image.tmdb.org/t/p/w185"
+        let posterPath = movie["poster_path"] as! String
+        let posterUrl = URL(string: baseUrl+posterPath)
+    
+        posterView.af_setImage(withURL: posterUrl!)
+        
+        let backdropPath = movie["backdrop_path"] as! String
+        let backdropUrl = URL(string: "http://image.tmdb.org/t/p/w780"+backdropPath)
+    
+        backdropView.af_setImage(withURL: backdropUrl!)
+                
     }
     
 
